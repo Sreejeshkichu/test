2fbc3dc0a9ecc0ece5fee6a043ec1c3430637089
Sreejeshkichu <155302340+Sreejeshkichu@users.noreply.github.com>
2025-02-17 09:35:49 +0530

    data.py

--- data.py
+++ data.py
@@ -1,8 +1,9 @@
 def  fun(x,y):
-    x = x+y
-    return x
+    a = x+y+z
+    return a
 
 num = int(input("enter the number :"))
 num1 = int(input("enter the number :"))
-result=(fun(num,num1))
+num2 = int(input("enter the number :"))
+result=(fun(num,num1,num2))
 print(result)
